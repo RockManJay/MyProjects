4ef199ec43922e2f334cfc14e0d1021cc8468ccc
RockManJay <mercury@Mercury-Linux.(none)>
2013-07-07 23:06:46 -0700

    Updated WebView project

--- TheNewBoston/src/com/jay/thenewboston/SimpleBrowser.java
+++ TheNewBoston/src/com/jay/thenewboston/SimpleBrowser.java
@@ -2,10 +2,16 @@ package com.jay.thenewboston;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SimpleBrowser extends Activity 
+public class SimpleBrowser extends Activity implements OnClickListener 
 {
+	EditText url;
+	WebView ourBrowser;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -13,8 +19,47 @@ public class SimpleBrowser extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.simplebrowser);
 		
-		WebView ourBrowser = (WebView) findViewById(R.id.wvBrowser);
+		ourBrowser = (WebView) findViewById(R.id.wvBrowser);
+		ourBrowser.setWebViewClient(new ourViewClient());
 		ourBrowser.loadUrl("http://www.mybringback.com");
+		
+		Button go = (Button) findViewById(R.id.bGo);
+		Button back = (Button) findViewById(R.id.bBack);
+		Button refresh = (Button) findViewById(R.id.bRefresh);
+		Button forward = (Button) findViewById(R.id.bForward);
+		Button clearHistory = (Button) findViewById(R.id.bClearHist);
+		url = (EditText) findViewById(R.id.etURL);
+		go.setOnClickListener(this);
+		back.setOnClickListener(this);
+		refresh.setOnClickListener(this);
+		forward.setOnClickListener(this);
+		clearHistory.setOnClickListener(this);		
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		switch (v.getId())
+		{
+		case R.id.bGo:
+			String theSite = url.getText().toString();
+			ourBrowser.loadUrl(theSite);
+			break;
+		case R.id.bBack:
+			if(ourBrowser.canGoBack())
+				ourBrowser.goBack();
+			break;
+		case R.id.bForward:
+			if(ourBrowser.canGoForward())
+				ourBrowser.goForward();
+			break;
+		case R.id.bRefresh:
+			ourBrowser.reload();
+			break;
+		case R.id.bClearHist:
+			ourBrowser.clearHistory();
+			break;
+		}
 	}
 
 }
