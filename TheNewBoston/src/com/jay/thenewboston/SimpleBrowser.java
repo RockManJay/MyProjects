b9a11128214df1f5b88769e894dbbfb9b9dc4e1b
RockManJay <mercury@Mercury-Linux.(none)>
2013-07-08 01:59:10 -0700

    Minor additions

--- TheNewBoston/src/com/jay/thenewboston/SimpleBrowser.java
+++ TheNewBoston/src/com/jay/thenewboston/SimpleBrowser.java
@@ -1,9 +1,14 @@
 package com.jay.thenewboston;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.URLUtil;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,8 +25,20 @@ public class SimpleBrowser extends Activity implements OnClickListener
 		setContentView(R.layout.simplebrowser);
 		
 		ourBrowser = (WebView) findViewById(R.id.wvBrowser);
+		
+		ourBrowser.getSettings().setJavaScriptEnabled(true);
+		ourBrowser.getSettings().setLoadWithOverviewMode(true);
+		ourBrowser.getSettings().setUseWideViewPort(true);
+		
 		ourBrowser.setWebViewClient(new ourViewClient());
-		ourBrowser.loadUrl("http://www.mybringback.com");
+		try
+		{
+			ourBrowser.loadUrl("http://www.mybringback.com");
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 		
 		Button go = (Button) findViewById(R.id.bGo);
 		Button back = (Button) findViewById(R.id.bBack);
@@ -33,7 +50,7 @@ public class SimpleBrowser extends Activity implements OnClickListener
 		back.setOnClickListener(this);
 		refresh.setOnClickListener(this);
 		forward.setOnClickListener(this);
-		clearHistory.setOnClickListener(this);		
+		clearHistory.setOnClickListener(this);
 	}
 
 	@Override
@@ -44,6 +61,9 @@ public class SimpleBrowser extends Activity implements OnClickListener
 		case R.id.bGo:
 			String theSite = url.getText().toString();
 			ourBrowser.loadUrl(theSite);
+			//Hiding the keyboard after using EditText
+			InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+			imm.hideSoftInputFromWindow(url.getWindowToken(), 0);
 			break;
 		case R.id.bBack:
 			if(ourBrowser.canGoBack())
